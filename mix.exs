4532be057e1bff659a342b77bd642ec78b6bac8a
nathanfaucett <nathanfaucett@gmail.com>
2017-03-11 10:45:45 -0600

    clean up

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LinksApi.Mixfile do
 
   def project do
     [app: :links_api,
-     version: "0.0.1",
+     version: (if Mix.env == :prod, do: System.get_env("APP_VERSION"), else: "0.0.1"),
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
